00602371eb05aa56eab2ac89c742d5f59834ae25
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-01 17:59:50 +0900

    Create week4_3.c

---
+++ week4_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int num1;
+  int num2;
+  int num3;
+  int total;
+  float average;
+  
+  scanf("%d %d %d", &num1, &num2, &num3);
+  total = num1 + num2 + num3;
+  average = total / 3;
+  printf("%d\n", total);
+  printf("%.2f", average);
+  return 0;
+  //codeup #1046
+}
