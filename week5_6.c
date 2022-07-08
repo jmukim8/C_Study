aad733e1f846da3b38d8d821ac8513422bf004ab
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-08 17:50:13 +0900

    Create week5_6.c

---
+++ week5_6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a,&b);
+	printf("%d\n", a == b);
+	//codeup 1057
+}
