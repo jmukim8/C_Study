0c32fe3629e41f99d31a4c08acd620411f69f4c9
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-08 17:35:07 +0900

    Create week5_4.c

---
+++ week5_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 250;
+	int b = 200;
+	int c = b + 100;
+	printf("%d\n", c - a == 60);
+	return 0;	
+}
+12페이지,1-3문제
