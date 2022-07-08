f8975d87f3beb0eb17591d1beeb5225cae7320a2
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-08 16:31:05 +0900

    Create w5-1.c

---
+++ w5-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, b;
+	
+	a = 23;
+	b = 24;
+	printf("%d\n", a > b);
+	printf("%d\n", a < b);
+	
+	return 0;
+}
