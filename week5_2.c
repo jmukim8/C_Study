04526ff10508f84d46fd119d5214cd9971e14e2e
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-08 17:02:22 +0900

    Create week5_2.c

---
+++ week5_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, b;
+	
+	printf("%d\n", 10 > 5 && 8 < 3);
+	printf("%d\n", 10 > 5 || 8 < 3);
+	printf("%d\n", !(10 > 8));
+	return 0;
+	
+}
