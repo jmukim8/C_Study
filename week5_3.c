d04ed4b04476eec7ec02a85c7ee99211a0cbce3e
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-08 17:15:03 +0900

    Create week5_3.c

---
+++ week5_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 500;
+	int b = 1000;
+	int c = 5000;
+	
+	printf("%d\n",c > a * 5 + b * 2);
+	return 0;
+	
+}
