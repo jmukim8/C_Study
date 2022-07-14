fc9246f9ac1d7c9c2525166646865bbc474bc171
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-14 17:04:15 +0900

    Create week5_9.c

---
+++ week5_9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 176;
+	int b = 67;
+  
+	printf("%d\n",a >= 170 && 95 >= b);
+	return 0;
+	
+}
+12페이지,1-4문제
