cd2a27c208a1c488dbc28e3006bcb53b24d4251b
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-14 17:07:12 +0900

    Create week5_10.c

---
+++ week5_10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 10;
+	int b = 10;
+  
+	printf("%d\n",10 > 8 && 10 >= 8);
+	return 0;
+	
+}
+12페이지,1-5문제
