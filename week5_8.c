ead5c8fc7a1db87275eac8154894cca40cd16403
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-14 16:54:17 +0900

    Create week5_8.c

---
+++ week5_8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 888;
+	int b = 1827;
+	int c = 972;
+  
+	printf("%d\n",1827 - a / 2  < 1827 - a);
+	return 0;
+	
+}
+12페이지,1-2문제
