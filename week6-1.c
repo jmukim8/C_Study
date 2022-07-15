77e21f7585ef4f918deee19ee068a38df0fdb1ed
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-15 16:40:52 +0900

    Create week6-1.c

---
+++ week6-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int a = 20;
+
+  a += 4;
+  printf("a = %d\n", a);
+  a /= 2;
+  printf("a = %d\n", a);
+  a--;
+  printf("a = %d\n", a);
+  a *= a;
+  printf("a = %d\n", a);
+}
+22쪽
