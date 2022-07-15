6e3fd8a17a629461cdd0b00003d1a2fa2246845f
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-15 17:50:48 +0900

    Create week6-4.c

---
+++ week6-4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int a;
+  scanf("%d", &a);
+  
+  if(a >= 90)
+  {  
+    printf("A");
+  }
+  else if(a >= 80)
+  {
+    printf("B");
+  }
+  else if(a >= 70)
+  {
+    printf("C");
+  }
+  else
+  {
+    printf("F");
+  }
+}
+36
