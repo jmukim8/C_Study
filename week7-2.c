9ce735defd3bbfd1bc9413a13c115fdc5095e02f
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-07-22 16:45:52 +0900

    Create week7-2.c

---
+++ week7-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int a;
+
+  scanf("%d", &a);
+
+  if(12 % a == 0 && 30 % a == 0)
+  {
+    printf("%d는 12와 30의 공약수다.\n",a);
+  }
+  else
+  {
+  	printf("%d는 12와 30의 공약수가 아니다.\n",a);
+  }
+  return 0;
+}
+42페이지,1-2문제
