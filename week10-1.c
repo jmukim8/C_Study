5af2d65452fc1ac8a7d84434eeb871a07879d292
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-08-19 16:41:03 +0900

    Create week10-1.c

---
+++ week10-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int n;
+  
+  printf("자연수 입력:");
+  scanf("%d",&n);
+  switch (n % 3)
+  {
+    case 0:
+      printf("삼의 배수가 맞음");break;
+    default:
+      printf("삼의 배수가 아님");break;
+  }
+  return 0;
+}
+23페이지
