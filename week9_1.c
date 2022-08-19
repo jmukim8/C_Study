706de08d3e3f603eaf37871a6a298f69d17f4b43
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-08-19 15:55:17 +0900

    Create week9_1.c

---
+++ week9_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  float n;
+  int day=0;
+
+  n = -19.40;
+  while(n < 0)
+  {
+    n = n+3;
+    n = n-2;
+    day++;
+  }
+  printf("%d",day);
+  return 0;
+}
+16페이지,1
