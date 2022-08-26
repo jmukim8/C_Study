100b14e7b44385236f0ba0b3d8a9e24d3773a028
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-08-26 17:29:47 +0900

    Create week10-8.c

---
+++ week10-8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int i,j;
+   
+  for(i = 5; i >= 1; i--)
+  {
+    for(j = i; j <= 5; j++)
+    {
+      printf("%d", j);
+    }    
+    printf("\n");
+  }
+  return 0;
+}
+43페이지,3
