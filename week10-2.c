942c633998eb6b1270a9c8dab98cc8b841a0f116
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-08-26 15:51:50 +0900

    Create week10-2.c

---
+++ week10-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int n;
+  
+  printf("자연수입력:");
+  scanf("%d", &n);
+
+  if (n % 2 == 0 && n <= 10)
+  {
+    printf("짝수");
+  }
+  if (n % 2 == 1 && n <= 10)
+  {
+    printf("홀수");
+  }
+  else if(n > 10)
+  {
+    printf("자연수");
+  }
+  return 0;
+}
