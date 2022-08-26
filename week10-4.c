5945002b0448cd85b00f1ce53e5639e0ecff6bcf
jmukim8 <108513244+jmukim8@users.noreply.github.com>
2022-08-26 17:27:32 +0900

    Create week10-4.c

---
+++ week10-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int i,j;
+  
+  for(i = 1; i <= 6; i++)
+  {
+    printf("%d학년 ", i);
+    for(j = 1; j <= 7; j++)
+    {
+      printf("%d반 ", j);
+    }    
+    printf("\n");
+  }
+  return 0;
+}
+38페이지
